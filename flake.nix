1c75a9197c74c9f3b46018de8588a22dc037d751
qhorgues <quentin.horgues@outlook.fr>
2026-03-14 22:47:57 +0100

    add darwin output

--- flake.nix
+++ flake.nix
@@ -10,6 +10,8 @@
       "x86_64-linux"
       "aarch64-linux"
       "i686-linux"
+      "x86_64-darwin" 
+      "aarch64-darwin"
     ] (system:
       let
         pkgs = import nixpkgs { inherit system; };
